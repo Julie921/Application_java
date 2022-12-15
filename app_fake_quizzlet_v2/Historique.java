cda01791dd3cd33b5f36f942af8977b53fef3c36
Delphine Nguyen <delphine.nguyen-durandet@sorbonne-nouvelle.fr>
2022-12-15 17:02:00 +0100

    Test pour la création de la classe Historique

--- app_fake_quizzlet_v2/Historique.java
+++ app_fake_quizzlet_v2/Historique.java
@@ -2,12 +2,10 @@ package app_fake_quizzlet_v2;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Map;
 
 public class Historique {
 
-    private ArrayList<HashMap<Exercice,Float>> data = new ArrayList<>();
+    private HashMap<Integer, ArrayList> data = new HashMap<>();
 
     //private HashMap<Integer, HashMap<Exercice, HashMap<ReponseEleve, Float>>> data = new HashMap<>();
 
