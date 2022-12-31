7290221f8add64100769bff047f0bef226a205dd
Delphine Nguyen <delphine.nguyen-durandet@sorbonne-nouvelle.fr>
2022-12-31 15:11:44 +0100

    Amélioration de la classe abstraite ReponseEleve et de la classe concrète ReponseEleveExoATrous : maintenant on met un lien vers l'exercice dans les attributs de ReponseEleve, ainsi qu'un lien vers l'élève ça permet de ne pas avoir à toujours renseigner l'élève et l'exercice alors qu'en soit, l'objet ReponseEleve est indissociable de l'élève et de l'exercice qu'il prend en argument dans le constructeur

--- app_fake_quizzlet_v2/ReponseEleve.java
+++ app_fake_quizzlet_v2/ReponseEleve.java
@@ -1,7 +1,29 @@
 package app_fake_quizzlet_v2;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public abstract class ReponseEleve {
 
-    public abstract void remplirReponse();
+    Exercice exercice; // l'exercice que l'élève fait
+    Eleve eleve; // l'élève qui fait l'exercice
+    ArrayList<ArrayList<String>> reponses = new ArrayList<>();
+
+    ReponseEleve(Exercice exercice, Eleve eleve){
+        this.exercice = exercice;
+        this.eleve = eleve;
+    }
+
+    public Exercice getExercice(){
+        return this.exercice;
+    }
+
+    public  Eleve getEleve(){
+        return this.eleve;
+    }
+
+    public ArrayList<ArrayList<String>> getReponses(){
+        return this.reponses;
+    }
 
 }
--- app_fake_quizzlet_v2/ReponseEleveExoATrous.java
+++ app_fake_quizzlet_v2/ReponseEleveExoATrous.java
@@ -5,48 +5,36 @@ import java.util.Scanner;
 
 public class ReponseEleveExoATrous extends ReponseEleve {
 
-    ArrayList<ArrayList<String>> mots = new ArrayList<>();
-    int note;
+    float note;
 
     ReponseEleveExoATrous(ExoATrous exercice, Eleve eleve){
 
-        System.out.println("Bonjour " + eleve.getPseudo());
+        super(exercice, eleve);
+
+        System.out.println("Bonjour " + eleve.getPseudo() + "!\n\n");
+
         exercice.afficheExercice();
 
-        Scanner myScanner = new Scanner(System.in);
+        Scanner myScanner = new Scanner(System.in); // scanner pour récupérer les réponses de l'élève
+
+        ArrayList<String> listeTampon = new ArrayList<>(); //on crée une liste tampon qu'on videra après chaque phrase
 
         int i = 1;
         for (PhraseATrous phrase : exercice.getListPhrases()) { //pour chaque phrase de l'exercice
-            ArrayList<String> listeTampon = new ArrayList<>(); //on crée une liste tampon
+            listeTampon.clear(); // on vide la liste tampon
             for (String mot : phrase.getMotsAPlacer()) { //pour chaque mot à placer dans la phrase
                 System.out.println("Quel est le mot manquant " + i + "?"); //on demande le mot manquant à la place X
                 String motDonne = myScanner.nextLine();
                 listeTampon.add(motDonne); //on ajoute notre mot à la liste tampon
                 i += 1;
             }
-            this.mots.add(listeTampon);
+            this.reponses.add(new ArrayList<String>(listeTampon));
         }
 
     }
 
-    public ArrayList<ArrayList<String>> getMots(){
-        return this.mots;
+    public void setNote(){
+        this.note = note;
     }
 
-    @Override
-    public void remplirReponse() {
-        /*exercice.affichePhraseAvecTrous(); //changer en getter
-        exercice.afficheMotsAPlacer(); //changer en getter
-
-        for (int i = 0; i < exercice.mots_a_placer.size(); i++) {
-            System.out.println("Quel est le mot manquant " + (i + 1) + "?");
-            Scanner myObj = new Scanner(System.in);
-            String motDonne = myObj.nextLine();
-            reponse_eleve.add(motDonne);
-        }
-
-        historique.append(reponse_eleve, exercice, pseudo); //pour remplir l'historique au fur et à mesure
-
-        return reponse_eleve;*/
-    }
 }
