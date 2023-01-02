40bfd7fe7c9e4f5122ed789a8a6d2fda31c71c55
Delphine Nguyen <delphine.nguyen-durandet@sorbonne-nouvelle.fr>
2023-01-02 11:00:27 +0100

    j'ai remis quelque chose que j'avais effacé par erreur et qui permettait de faire fonctionner ORMlite correctement lors de la création des Eleve et des Profs

--- app_fake_quizzlet_v2/Utilisateur.java
+++ app_fake_quizzlet_v2/Utilisateur.java
@@ -1,9 +1,19 @@
 package app_fake_quizzlet_v2;
 
+import com.j256.ormlite.field.DatabaseField;
+
 public abstract class Utilisateur {
 
     /* Le pseudo de l'utilisateur*/
+    @DatabaseField(id = true)
     private String pseudo;
+
+    /**
+     * Constructeur vide de la classe Utilisateur nécessaire pour utiliser ORMlite lors de la création des classes concrètes qui étendent Utilisateur.
+     * @see Eleve
+     * @see Professeur
+     */
+    public Utilisateur(){}
 
     /**
      * Constructeur de la classe Utilisateur.
