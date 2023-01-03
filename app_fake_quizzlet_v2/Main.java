856ba0c2108461f4ee07f5497c2e638988c09963
Delphine Nguyen <delphine.nguyen-durandet@sorbonne-nouvelle.fr>
2023-01-04 00:41:39 +0100

    amélioration des boucles jusqu'à la fin de l'identification

--- app_fake_quizzlet_v2/Main.java
+++ app_fake_quizzlet_v2/Main.java
@@ -7,6 +7,7 @@ import com.j256.ormlite.logger.LogBackendType;
 import com.j256.ormlite.logger.LoggerFactory;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
+import org.fusesource.jansi.Ansi;
 
 import javax.swing.*;
 import java.sql.SQLException;
@@ -17,6 +18,9 @@ public class Main {
 
     private final static String DATABASE_URL = "jdbc:h2:file:./database.db";
 
+    /**
+     * Dossier dans lequel se trouve les ressources de l'application = les fichiers .txt contenant des exercices
+     */
     public final static String RESSOURCES_FOLDER = "./ressources";
     private static ConnectionSource connectionSource = null;
     private static Dao<Professeur, String> professeurDao;
@@ -64,7 +68,7 @@ public class Main {
             eleveDao = DaoManager.createDao(connectionSource, Eleve.class);
             niveauElevesDao = DaoManager.createDao(connectionSource, NiveauxEleves.class);
 
-            // if you need to create the table
+            // création des tables dans la base de données pour les objets Professeur, Eleve et NiveauxEleves
             TableUtils.createTableIfNotExists(connectionSource, Professeur.class);
             TableUtils.createTableIfNotExists(connectionSource, Eleve.class);
             TableUtils.createTableIfNotExists(connectionSource, NiveauxEleves.class);
@@ -72,48 +76,61 @@ public class Main {
             ///////////////////////////////////////////////////////////////////////
 
 
-            createFromDatabase();
-            remplirListeParseurs();
+            createFromDatabase(); // création des objets Eleve, Prof et NiveuxEleves depuis les infos de la database
+            remplirListeParseurs(); // création des parseurs
+
+            // création de l'objet pour importer des exercices
             importExercice = new ImportExercice(listParseurs);
-            listExercices = importExercice.importDossier(RESSOURCES_FOLDER);
+            listExercices = importExercice.importDossier(RESSOURCES_FOLDER); // récupération des exercices qui sont dans le dossier de ressources de l'application
 
-            Boolean professeurSession = false, studentSession = false;
-            String inputUser = "";
+            String inputUser = ""; // initilisation de la string qui contiendra ce que l'utilisateur rentre
 
-            do {
-                System.out.println("Bonjour ! Vous êtes un : \n - 1 : élève\n - 2 : professeur");
+            //Cette boucle permet à l'utilisateur de choisir son type de session (élève ou professeur).
+             //Si l'utilisateur entre 1, la variable studentSession sera mise à true. Si l'utilisateur entre 2, la variable professeurSession sera mise à true.
+             //Si l'utilisateur entre !quit, la méthode fermetureConnexion sera appelée et le programme se termine.
+             //Si l'utilisateur entre une autre réponse, il sera informé que sa réponse ne convient pas.
+             //La boucle se répète jusqu'à ce que l'utilisateur entre 1, 2 ou !quit.
+
+            boolean studentSession = false;
+            boolean professeurSession = false;
+
+            while (!studentSession && !professeurSession) { // tant qu'on ne sait pas si c'est un étudiant ou un prof
+                System.out.println("Bonjour ! Vous êtes un : \n " +
+                        "- 1 : élève\n " +
+                        "- 2 : professeur");
                 System.out.print("Votre réponse: ");
                 inputUser = scannerInputUser.nextLine();
 
-                // Choix Session
-                studentSession = (inputUser.equals("1"));
-                professeurSession = (inputUser.equals("2"));
-                if (!studentSession && !professeurSession && !inputUser.equals(QUIT_COMMAND)) {
-                    System.out.println("Votre réponse ne convient pas");
-                }
+                if (inputUser.equals("1")) { // c'est un élève
+                    studentSession = true;
+                } else if (inputUser.equals("2")) { // c'est un prof
+                    professeurSession = true;
+                } else if (!inputUser.equals(QUIT_COMMAND)) { // input différent de "1", "2" ou "!quit"
+                    System.out.println(Ansi.ansi().fg(Ansi.Color.RED).a("\n/!\\ Votre réponse ne convient pas.\n").reset());
 
-                if (inputUser.equals(QUIT_COMMAND)) {
+                } else { // l'utilisateur a rentré la commande pour quitter
+                    fermetureConnexion();
+                    return;
+                }
+            }
+
+            // boucle pour s'identifier
+
+            Utilisateur utilisateurActif = null; // l'utilisateur qui utilise l'application ne s'est pas encore identifié
+
+            while (utilisateurActif == null) { // tant que l'utilisateur ne s'est pas identifié
+                System.out.println("\nQuel est votre identifiant ?");
+                System.out.print("Identifiant/pseudo : ");
+                inputUser = scannerInputUser.nextLine();
+
+                if (inputUser.equals(QUIT_COMMAND)) { // l'utilisateur veut quitter l'application
                     fermetureConnexion();
                     return;
                 }
 
-            } while (!inputUser.equals(QUIT_COMMAND) && !studentSession && !professeurSession);
-
-            do {
-                System.out.println("\nQuel est votre identifiant ?");
-                System.out.print("Idenfitiant/pseudo: ");
-                inputUser = scannerInputUser.nextLine();
-
-                if (inputUser.equals(QUIT_COMMAND))
-                    break;
-
-                if (professeurSession) { //TODO : changer et mettre dans Utilisateur ?
-                    utilisateurActif = checkSession(inputUser, "prof");
-                } else if (studentSession) {
-                    utilisateurActif = checkSession(inputUser, "eleve");
-                }
-
-            } while (utilisateurActif == null);
+                String type = studentSession ? "eleve" : "prof"; // si studentSession est true, la valeur du type est "eleve", si studentSession est false, la valeur est "prof"
+                utilisateurActif = checkSession(inputUser, type); // l'utilisateur s'identifie
+            }
 
             if (utilisateurActif == null) {
                 fermetureConnexion();
@@ -306,110 +323,161 @@ public class Main {
         // Rajouter nouveaux parseurs ici si on veut créer d'autres types d'exercices
     }
 
+
     /**
-     * Cette méthode vérifie si l'utilisateur actif (un élève ou un professeur) existe dans la base de données et, le cas échéant, renvoie l'objet correspondant.
-     * Si l'utilisateur n'existe pas, l'utilisateur peut choisir de s'enregistrer.
-     * Si l'utilisateur est un élève, il doit en outre sélectionner ses professeurs parmi ceux existants dans la base de données.
-     *
-     * @param pseudo le pseudo de l'utilisateur actif
-     * @param type   le type d'utilisateur actif ("prof" ou "eleve")
-     * @return l'objet Utilisateur correspondant à l'utilisateur actif
+     *  Cette méthode vérifie si l'identifiant d'un professeur ou d'un élève existe déjà dans la base de données.
+     *  Si l'identifiant existe, l'utilisateur correspondant devient l'utilisateur actif.
+     *  Si l'identifiant n'existe pas, l'utilisateur est invité à se créer un compte.
+     *  Si l'utilisateur décide de se créer un compte, il est redirigé vers la fonction de création de compte correspondant à son type (professeur ou élève).
+     *  Si l'utilisateur décide de ne pas se créer de compte, la fonction retourne null.
+     *  @param pseudo l'identifiant de l'utilisateur à vérifier
+     *  @param type le type d'utilisateur à vérifier ("prof" ou "eleve")
+     *  @return l'utilisateur actif ou null si l'utilisateur n'a pas choisi de se créer de compte
      */
-    public static Utilisateur checkSession(String pseudo, String type) {
+    public static Utilisateur checkSession(String pseudo, String type) throws Exception {
         String inputUser = "";
-        Boolean choix1, choix2 = false;
         Utilisateur user = null;
-        try {
+
+        try { // vérification de si le prof ou l'élève existe déjà dans la BDD
             if (type.equals("prof")) {
                 user = professeurDao.queryForId(pseudo);
             } else if (type.equals("eleve")) {
                 user = eleveDao.queryForId(pseudo);
             }
 
-            if (user != null) {
+            if (user != null) { // si l'utilisateur existe, il devient l'utilisateur actif
                 return user;
             }
-
-            // L'utilisateur n'existe pas dans la DB
-            System.out.println("/!\\ Identifiant inconnu ! Voulez-vous vous le créer ? (pseudo : " + pseudo + ")");
-            System.out.println("1 : Oui");
-            System.out.println("2 : Non");
-            inputUser = scannerInputUser.nextLine();
-
-            // Choix User
-            choix1 = inputUser.equals("1");
-            choix2 = inputUser.equals("2");
-
-            if (!choix1 && !choix2) {
-                System.out.println("Votre réponse ne convient pas\n//-------------------------------------------------//");
-            }
-
-            // L'utilisateur veut s'enregistrer
-            if (choix1) {
-                // Si l'utilisateur n'existe pas, création.
-                if (type.equals("prof")) {
-
-                    // Langue
-                    System.out.println("Quelle langue enseignez-vous ?");
-                    List<Langue> allLangues = Arrays.asList(Langue.values());
-                    for (int i = 0; i < allLangues.size(); i++) {
-                        System.out.println((i + 1) + ": " + allLangues.get(i));
-                    }
-                    System.out.print("Langue choisie: ");
-                    inputUser = scannerInputUser.nextLine();
-                    int indexLangue = Integer.parseInt(inputUser) - 1;
-                    if (((indexLangue) < 0) || (indexLangue >= allLangues.size())) {
-                        System.out.println("Saisie invalide, index non compris");
-                        return null;
-                    }
-
-                    user = new Professeur(pseudo, allLangues.get(indexLangue));
-                    professeurDao.create((Professeur) user);
-                } else {
-                    // Creation de l'utilsateur principal
-                    user = new Eleve(pseudo);
-                    eleveDao.create((Eleve) user);
-
-                    Boolean profsFini = false;
-                    do {
-                        List<Professeur> allProf = professeurDao.queryForAll();
-                        System.out.println("Qui sont vos professeurs parmi ceux-ci ?\n" +
-                                "Note: Rentrer la valeur 0 lorsque vous avez fini de choisir tous vos professeurs.");
-                        for (int i = 0; i < allProf.size(); i++) {
-                            Professeur p = allProf.get(i);
-                            System.out.println((i + 1) + ": " + p.getPseudo() + " - " + p.getLangue());
-                        }
-                        System.out.print("Professeur choisi: ");
-                        inputUser = scannerInputUser.nextLine();
-                        int indexProf = Integer.parseInt(inputUser) - 1;
-
-                        // Si l'eleve a rentrée 0 dans sa donnée
-                        // il a fini de choisir ses professeurs
-                        if (indexProf == -1) {
-                            profsFini = true;
-                        } else {
-                            // Sinon, on vérifie que l'index donné est correct
-                            if ((indexProf < 0) || (indexProf >= allProf.size())) {
-                                System.out.println("Saisie invalide, index non compris.");
-                                return null;
-                            }
-                            // et on l'ajoute dans la liste des profs de l'eleve.
-                            Professeur prof = allProf.get(indexProf);
-                            ((Eleve) user).inscriptionLangue(prof, listNiveauxUtilisateur, niveauElevesDao);
-                        }
-                    } while (!profsFini);
-                }
-
-            }
-            // Le prof ne veut pas s'enregistrer
-            else if (choix2) {
-                System.out.println("WARNING: Vous ne pouvez pas utiliser l'application sans créer de compte.");
-            }
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
 
-        return user;
+        while (true) { // boucle pour demander si l'utilisateur veut créer un compte vu que son identifiant n'existe pas
+            System.out.println(Ansi.ansi().fg(Ansi.Color.RED).a("\n/!\\ L'identifiant '" + pseudo + "' n'existe pas. Voulez-vous vous le créer ?\n").reset());
+            System.out.println("1 : Oui");
+            System.out.println("2 : Non");
+            System.out.print("Création : ");
+            inputUser = scannerInputUser.nextLine();
+
+            if (inputUser.equals("1")) { // l'utilisateur veut créer un compte
+                if (type.equals("prof")) { // si c'est un prof, on crée un prof
+                    user = createProf(pseudo);
+                } else { // si c'est un élève, on crée un élève
+                    user = createEleve(pseudo);
+                }
+                return user; // l'utilisateur créé devient l'utilisateur actif
+            }
+
+            else if (inputUser.equals("2")) { // l'utilisateur ne veut pas créer de compte
+                System.out.println(Ansi.ansi().fg(Ansi.Color.RED).a("\n/!\\ Vous ne pouvez pas utiliser l'application sans vous identifier.\n").reset());
+                return null;
+            }
+
+            else { // le choix n'est pas reconnu
+                System.out.println(Ansi.ansi().fg(Ansi.Color.RED).a("\n/!\\ Votre réponse ne convient pas.\n").reset());
+            }
+        }
+    }
+
+    /**
+     *  Cette méthode permet de créer un professeur dans la base de données.
+     *  L'utilisateur est invité à saisir la langue qu'il souhaite enseigner.
+     *  Si la saisie de l'utilisateur est incorrecte ou n'est pas comprise dans les valeurs possibles, l'utilisateur est invité à ressaisir sa réponse.
+     *  Si l'utilisateur entre la commande !quit, la méthode fermetureConnexion est appelée et la méthode actuelle retourne null.
+     *  @param pseudo Le pseudo du professeur à créer
+     *  @return L'objet Professeur créé
+     */
+    public static Professeur createProf(String pseudo) {
+        Professeur prof = null;
+        String inputUser = "";
+        try {
+            while (prof == null) { // tant que le prof n'est pas créé
+                // choix de la langue enseignée (il ne peut enseigner qu'une seule langue)
+                System.out.println("\nQuelle langue enseignez-vous ?");
+                List<Langue> allLangues = Arrays.asList(Langue.values()); //affichage de la liste des langues disponibles
+                for (int i = 0; i < allLangues.size(); i++) {
+                    System.out.println((i + 1) + " : " + allLangues.get(i));
+                }
+                System.out.print("Langue choisie : ");
+                inputUser = scannerInputUser.nextLine();
+
+                if(inputUser.equals(QUIT_COMMAND)){
+                    System.out.println(Ansi.ansi().fg(Ansi.Color.RED).a("\n/!\\ Votre identifiant ne sera pas créé.\n").reset());
+                    fermetureConnexion();
+                    return null;
+                }
+
+                int indexLangue = Integer.parseInt(inputUser) - 1;
+
+                if (((indexLangue) < 0) || (indexLangue >= allLangues.size())) { // si le numéro n'est pas compris dans la liste des langues
+                    System.out.println(Ansi.ansi().fg(Ansi.Color.RED).a("\n/!\\ Saisie invalide.\n").reset());
+                } else { // si le numéro est bon, on crée le prof
+                    prof = new Professeur(pseudo, allLangues.get(indexLangue));
+                    professeurDao.create(prof);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return prof;
+    }
+
+    /**
+     * Méthode permettant la création d'un nouvel élève et son inscription dans les cours de ses professeurs.
+     * Si la saisie de l'utilisateur est incorrecte ou n'est pas comprise dans les valeurs possibles, l'utilisateur est invité à ressaisir sa réponse.
+     * Si l'utilisateur entre la commande !quit, la méthode fermetureConnexion est appelée et la méthode actuelle retourne null.
+     * @param pseudo le pseudo de l'élève
+     * @return l'élève créé
+     */
+    public static Eleve createEleve(String pseudo) {
+        Eleve eleve = null;
+        String inputUser = "";
+        try {
+            eleve = new Eleve(pseudo);
+            eleveDao.create(eleve);
+
+            Boolean profsFini = false;
+            do { // tant que l'élève n'a pas fini de rentrer tous ses profs
+                List<Professeur> allProf = professeurDao.queryForAll(); // on affiche la liste des professeurs avec la langue qu'ils enseignent
+                System.out.println("\nQui sont vos professeurs parmi ceux-ci ?\n" +
+                        "NOTE : Entrez '0' quand vous avez rentré tous vos professeurs.\n");
+                for (int i = 0; i < allProf.size(); i++) {
+                    Professeur p = allProf.get(i);
+                    System.out.println((i + 1) + " : " + p.getPseudo() + " - " + p.getLangue());
+                }
+                System.out.print("\nProfesseur choisi: ");
+                inputUser = scannerInputUser.nextLine();
+
+                if(inputUser.equals(QUIT_COMMAND)){
+                    System.out.println(Ansi.ansi().fg(Ansi.Color.RED).a("\n/!\\ Votre compte est créé. Vous pourrez vous inscrire des cours plus tard.\n").reset());
+                    fermetureConnexion();
+                    return null;
+                }
+
+                int indexProf = Integer.parseInt(inputUser) - 1;
+
+                // Si l'eleve a rentrée 0, il a fini de choisir ses professeurs
+                if (indexProf == -1) {
+                    profsFini = true;
+                } else {
+                    // Sinon, on vérifie que l'index donné est correct
+                    if ((indexProf < 0) || (indexProf >= allProf.size())) {
+                        System.out.println(Ansi.ansi().fg(Ansi.Color.RED).a("\n/!\\ Saisie invalide, index non compris.\n").reset());
+                    } else { // si il est correct
+                        // On inscrit l'élève dans le cours du professeur
+                        Professeur prof = allProf.get(indexProf);
+                        eleve.inscriptionLangue(prof, listNiveauxUtilisateur, niveauElevesDao);
+                    }
+                }
+            } while (!profsFini);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return eleve;
     }
 
     /**
